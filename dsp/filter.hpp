2f9788231aa4c5960c05b6eb4cbacfc4d4aa1cab
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-09-25 11:07:48 +0200

    combfilter

--- dsp/filter.hpp
+++ dsp/filter.hpp
@@ -1,65 +1,28 @@
 #include "buffer.hpp"
 
 namespace dspheaders {
-  namespace filter {
-    class Comb {
-      protected:
-        Buffer buffer;
-        float previn = 0.00001f;
-        float prevout = 0.00001f;
-        float read(unsigned readptr);
-        void write(float sample);
-        unsigned writeptr = 0;
-        float readptr = 0.f;
+  class Comb {
+    protected:
+      Buffer buffer;
+      float previn = 0.00001f;
+      float prevout = 0.00001f;
+      float read(float readptr);
+      void write(float sample);
+      unsigned writeptr = 0;
+      float readptr = 0.f;
 
-      public: 
-        virtual float play(float sample, float feedback);
-        virtual float play(float sample, float feedback, float mod);
-        Comb(
-            unsigned offset,
-            unsigned samplerate,
-            float (*interpolate)(float, float*, unsigned)
-        );
-    };
-
-///////////////////////////////////////////////////////////////////////////////
-/// COMB IIR
-///////////////////////////////////////////////////////////////////////////////
-
-    class CombIIR : public Comb {
-      CombIIR (
-        unsigned offset,
-        unsigned samplerate,
-        float (*interpolate)(float, float*, unsigned)
-      );
-    };
-
-///////////////////////////////////////////////////////////////////////////////
-/// COMB FIR
-///////////////////////////////////////////////////////////////////////////////
-
-    class CombFIR : public Comb {
-      float play(float sample, float feedback);
-      float play(float sample, float feedback, float mod);
-
-      CombFIR (
+    public: 
+      // Feedback
+      float playIIR(float sample, float feedback);
+      float playIIR(float sample, float feedback, float mod);
+      // Feedforward
+      float playFIR(float sample, float amp);
+      float playFIR(float sample, float amp, float mod);
+      Comb(
           unsigned offset,
           unsigned samplerate,
           float (*interpolate)(float, float*, unsigned)
       );
-    };
-
-
-
-    class Allpass : public Comb {
-        float play(float sample, float feedback);
-        float play(float sample, float feedback, float mod);
-        Allpass (
-            unsigned offset,
-            unsigned samplerate,
-            float (*interpolate)(float, float*, unsigned)
-        );
-
-    };
-  }
+  };
 }
+

04af8e29d047db0144aad2857a70cbebedf5f848
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-09-25 11:07:48 +0200

    combfilter

--- dsp/filter.cpp
+++ dsp/filter.cpp
@@ -4,13 +4,13 @@
 #include "interpolation.hpp"
 #include <cstdio>
 
-using namespace dspheaders::filter;
+using namespace dspheaders;
 
 //////////////////////////////////////////////////////////////////////////////
 /// COMB FEEDBACK
 //////////////////////////////////////////////////////////////////////////////
 
-float Comb::read(unsigned readptr) {
+float Comb::read(float readptr) {
   // in a delay, we read at [0+n] and write at [0.f + n + offset]
   float readsample = buffer.readsample(readptr);
   return readsample;
@@ -20,34 +20,33 @@ void Comb::write(float sample) {
   buffer.writesample(sample, writeptr);
 }
 
-float Comb::play(float sample, float feedback) {
+float Comb::playIIR(float sample, float feedback) {
   float output = 0.f;
   float dly = read(readptr);
   // readptr reads dragging behind writeptr
-  float out = sample - (dly * feedback);
+  float out = sample + (dly * feedback);
   // output = interpolation::slope(prevout, out);
   //dcblock
-  output = dcblock(output, previn, prevout);
+  // output = dcblock(output, previn, prevout);
 
-  previn = sample;
-  prevout = out;
+  // previn = sample;
+  // prevout = out;
 
   write(out);
   readptr+=1.f; 
   writeptr++;
-  return output;
+  return out;
 }
 
-float Comb::play(float sample, float feedback, float mod) {
+float Comb::playIIR(float sample, float feedback, float mod) {
   float output = 0.f;
   float dly = read(readptr);
   // readptr reads dragging behind writeptr
-  float out = sample - (dly * feedback);
+  float out = sample + (dly * feedback);
 
   // when modulating the readptr, we need to do
   // a simple "interpolation", using the mean value
   output = interpolation::slope(prevout, out);
-  // dcblock
   output = dcblock(output, previn, prevout);
 
   // store previous in and out
@@ -58,126 +57,55 @@ float Comb::play(float sample, float feedback, float mod) {
   readptr += 1.f + mod; 
   writeptr++;
   return output;
+}
+
+
+float Comb::playFIR(float sample, float amp) {
+  float output = 0.f;
+  float dly = read(readptr);
+  write(sample);
+  // readptr reads dragging behind writeptr
+  float out = sample + (dly * amp);
+  // output = interpolation::slope(prevout, out);
+  //dcblock
+  // output = dcblock(output, previn, prevout);
+
+  // previn = sample;
+  // prevout = out;
+
+  readptr+=1.f; 
+  writeptr++;
+  return out;
+}
+
+float Comb::playFIR(float sample, float amp, float mod) {
+  float output = 0.f;
+  float dly = read(readptr);
+  write(sample);
+  // readptr reads dragging behind writeptr
+  float out = sample + (dly * amp);
+
+  // when modulating the readptr, we need to do
+  // a simple "interpolation", using the mean value
+  // output = interpolation::slope(prevout, out);
+  // dcblock
+  // output = dcblock(output, previn, prevout);
+
+  // store previous in and out
+  // previn = sample;
+  // prevout = out;
+
+  readptr += 1.f + mod; 
+  writeptr++;
+  return out;
+  // TODO
+  return 0.f;
 }
 
 Comb::Comb(
     unsigned offset,
     unsigned samplerate,
     float (*interpolate)(float, float*, unsigned))
-  : readptr((4*samplerate) - offset), 
-    buffer(Buffer(4*samplerate, samplerate, interpolate)) {
-}
+  : buffer(Buffer(4*samplerate, samplerate, interpolate)),
+    readptr((float)(buffer.bufferlength - offset)) { }
 
-// Naming wrapper around Comb class, which is as default a IIR Comb filter
-CombIIR::CombIIR(
-  unsigned offset,
-  unsigned samplerate,
-  float (*interpolate)(float, float*, unsigned)) 
-  : Comb(offset, samplerate, interpolate) {
-};
-
-//////////////////////////////////////////////////////////////////////////////
-/// ALLPASS 
-//////////////////////////////////////////////////////////////////////////////
-
-Allpass::Allpass(
-    unsigned offset,
-    unsigned samplerate,
-    float (*interpolate)(float, float*, unsigned))
-  : 
-    Comb(offset, samplerate, interpolate)
-{}
-
-float Allpass::play(float sample, float feedback) {
-  float output = 0.f;
-  float dly = read(readptr);
-  // readptr reads dragging behind writeptr
-  float out = sample - (dly * feedback);
-  // output = interpolation::slope(prevout, out);
-  //dcblock
-  output = dcblock(output, previn, prevout);
-
-  previn = sample;
-  prevout = out;
-
-  write(out);
-  readptr+=1.f; 
-  writeptr++;
-  return output;
-}
-
-
-float Allpass::play(float sample, float feedback, float mod) {
-  float output = 0.f;
-  float dly = read(readptr);
-  // readptr reads dragging behind writeptr
-  float out = sample - (dly * feedback);
-
-  // when modulating the readptr, we need to do
-  // a simple "interpolation", using the mean value
-  output = interpolation::slope(prevout, out);
-  // dcblock
-  output = dcblock(output, previn, prevout);
-
-  // store previous in and out
-  previn = sample;
-  prevout = out;
-
-  write(out);
-  readptr += 1.f + mod; 
-  writeptr++;
-  return output;
-}
-      
-CombFIR::CombFIR (
-  unsigned offset,
-  unsigned samplerate,
-  float (*interpolate)(float, float*, unsigned))
-  : Comb(offset, samplerate, interpolate) {
-};
-
-float CombFIR::play (float sample, float wet) {
-  float output = 0.f;
-
-  // write input first
-  write(sample);
-  // read later
-  float dly = read(readptr);
-  // Combine input and read buffer sample
-  output = sample + (dly * wet);
-  // Slope 
-  // output = interpolation::slope(prevout, out);
-  // dcblock
-  // output = dcblock(output, previn, prevout);
-
-  // previn = sample;
-  // prevout = output;
-
-  readptr+=1.f; 
-  writeptr++;
-  return output;
-}
-
-float CombFIR::play (float sample, float wet, float mod) {
-  float output = 0.f;
-  // write input first
-  write(sample);
-  // read delay
-  float dly = read(readptr);
-
-  float out = sample + (dly * wet);
-
-  // when modulating the readptr, we need to do
-  // a simple "interpolation", using the mean value
-  output = interpolation::slope(prevout, out);
-  // dcblock
-  output = dcblock(output, previn, prevout);
-
-  // store previous in and out
-  previn = sample;
-  prevout = out;
-
-  readptr += 1.f + mod; 
-  writeptr++;
-  return output;
-}

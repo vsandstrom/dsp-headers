535b50772d5dbf26f6d642aca74fa8174a5df453
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-12-13 18:07:05 +0100

    grains with speed

--- dsp/grain.cpp
+++ dsp/grain.cpp
@@ -1,5 +1,6 @@
 #include "grain.hpp"
 #include <cmath>
+#include <cstdlib>
 
 using namespace dspheaders;
 /*
@@ -14,12 +15,30 @@ float Grain::play(float readposition) {
   // set function variable with position in delay buffer
   float out = 0.f;
   // If envelope has run its course, 'active' is set to 0 ( falsy )
-  if (active) {
-    float ptr = fmod(
-      readptr + (unsigned)(readposition * buffer->bufferlength), 
-      buffer -> bufferlength
-    );
-    readptr+=1;
+  if (active) { 
+    // set randomness ( jitter ) as value of seconds 
+    float pos = readptr + (readposition * buffer->bufferlength);
+    float ptr = fmod(pos, buffer -> bufferlength);
+    readptr+=playbackspeed;
+    // icrement readptr - Should mirror Granulator readptr
+    out = buffer->readsample(ptr)*envelope->play(graindur);
+    active = envelope->running();
+  }
+  return out;
+}
+
+// readposition travels from 0 -> 1 
+float Grain::play(float readposition, float speed) {
+  setSpeed(speed);
+
+  // set function variable with position in delay buffer
+  float out = 0.f;
+  // If envelope has run its course, 'active' is set to 0 ( falsy )
+  if (active) { 
+    // set randomness ( jitter ) as value of seconds 
+    float pos = readptr + (readposition * buffer->bufferlength);
+    float ptr = fmod(pos, buffer -> bufferlength);
+    readptr+=playbackspeed;
     // icrement readptr - Should mirror Granulator readptr
     out = buffer->readsample(ptr)*envelope->play(graindur);
     active = envelope->running();
@@ -29,6 +48,10 @@ float Grain::play(float readposition) {
 
 void Grain::setDur(float dur) {
   graindur = (envlength / dur) * samplerate;
+}
+
+void Grain::setSpeed(float speed) {
+  playbackspeed = speed;
 }
 
 Grain::Grain(
